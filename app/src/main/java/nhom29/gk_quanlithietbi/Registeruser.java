213b5d8fcf3a7dc9228d21f913bde1b6633c9510
tran quang linh <99276011+linhtran-281098@users.noreply.github.com>
2022-05-03 20:40:48 +0700

    fix

---
+++ app/src/main/java/nhom29/gk_quanlithietbi/Registeruser.java
@@ -0,0 +1,14 @@
+package nhom29.gk_quanlithietbi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registeruser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeruser);
+    }
+}
