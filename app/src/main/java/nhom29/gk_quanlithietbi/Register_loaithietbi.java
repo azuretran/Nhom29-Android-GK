22ac21cc50c9c70aaa959caa20c18df88274f7b0
tran quang linh <99276011+linhtran-281098@users.noreply.github.com>
2022-05-06 14:17:33 +0700

    fix2

---
+++ app/src/main/java/nhom29/gk_quanlithietbi/Register_loaithietbi.java
@@ -0,0 +1,14 @@
+package nhom29.gk_quanlithietbi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_loaithietbi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_loaithietbi);
+    }
+}
